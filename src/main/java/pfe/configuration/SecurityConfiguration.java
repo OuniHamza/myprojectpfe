fd84bf10b4cceae2a4f1a8f3cfe7367f338fcee0
hamzaouni100@gmail.com <hamza.ellouni@gmail.com>
2018-03-25 15:15:10 +0200

    Initial commit

---
+++ src/main/java/pfe/configuration/SecurityConfiguration.java
@@ -0,0 +1,43 @@
+package pfe.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.encoding.Md5PasswordEncoder;
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+import javax.sql.DataSource;
+
+//@EnableGlobalMethodSecurity(securedEnabled = true)
+@EnableWebSecurity
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
+    @Autowired
+    DataSource dataSource;
+//     @Autowired
+//     private UserDetailsService userDetailsService;
+
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication().dataSource(dataSource)
+                .usersByUsernameQuery(
+                        "select username as principal,password as credentials , true from users where username=?")
+                .authoritiesByUsernameQuery(
+                        "select username as principal ,role as role from users where username=?")
+                .rolePrefix("ROLE_");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+         http.csrf().disable();
+//                http.authorizeRequests();
+       http.authorizeRequests().anyRequest().authenticated().and().formLogin().permitAll();
+//      //  http.exceptionHandling().accessDeniedPage("/403");
+    }
+}
---
+++ src/main/java/pfe/service/RoleServiceImpl.java
@@ -0,0 +1,11 @@
+package pfe.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class RoleServiceImpl implements RoleService {
+
+    @Override
+    public void ajouterRoleToUser(String role) {
+
+    }
+}
